489e9371fe9956d2e8204c91f4cd73009340dc80
jannekaeder <46530448+jannekaeder@users.noreply.github.com>
2019-04-16 11:51:08 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+
+alert('hello, this window is useless. just close it.');
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
